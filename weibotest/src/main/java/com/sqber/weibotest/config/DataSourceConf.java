d977480a57e550e436e10acf13b35114d988918d
shenqiangbin <1348907384@qq.com>
2021-03-29 23:39:19 +0800

    feat: 配置 MySQL 数据库

---
+++ weibotest/src/main/java/com/sqber/weibotest/config/DataSourceConf.java
@@ -0,0 +1,48 @@
+package com.sqber.weibotest.config;
+
+import com.sqber.weibotest.db.DBHelper;
+//import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataSourceConf {
+
+    @Bean
+    @Primary
+    @ConfigurationProperties(prefix = "spring.datasource.mysql")
+    public DataSource dataSource() {
+        return DataSourceBuilder.create().build();
+    }
+
+    @Bean
+    @Primary
+    public DBHelper primaryDbHelper(DataSource dataSource) {
+        return new DBHelper(dataSource);
+    }
+
+    // 多数据源：在有一个数据源时，配置如下
+//    @Bean("secondDataSource")
+//    @ConfigurationProperties(prefix = "spring.datasource.psg") // application.properteis中对应属性的前缀
+//    public DataSource dataSource2() {
+//        return DataSourceBuilder.create().build();
+//    }
+//
+//    @Bean("postgre")
+//    public DBHelper postgreSqlHelper(@Qualifier("secondDataSource") DataSource dataSource) {
+//        return new DBHelper(dataSource);
+//    }
+
+//    /**
+//     *     其他地方使用：
+//     *     @Autowired
+//     *     @Qualifier("postgre")
+//     *     private DBHelper postgreHelper;
+//     */
+
+}
